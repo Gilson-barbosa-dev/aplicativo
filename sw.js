d756d892d172b7e782ee712dbc9abdb0c0f5af8c
Gilson Barbosa <gilsonbarbosa.s@gmail.com>
2025-08-13 14:51:01 -0300

    Ajustado

--- sw.js
+++ sw.js
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-self.addEventListener('install', event => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
-=======
 self.addEventListener("install", event => {
   console.log("📥 Service Worker instalado");
+  self.skipWaiting(); // Garante que o SW ativo substitua versões antigas imediatamente
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(fetch(event.request));
 });
->>>>>>> 27f76e718e7c1eaf22d3bf5492260c6b0e832330
