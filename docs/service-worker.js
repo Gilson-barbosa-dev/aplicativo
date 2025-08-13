f4e60a8864fecf541c250ac929fe022ff04f949e
Gilson Barbosa <gilsonbarbosa.s@gmail.com>
2025-08-13 17:32:03 -0300

    Ajuste5

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,28 +1,24 @@
-const CACHE_NAME = "top-musicas-cache-v2";
+const CACHE_NAME = "top-musicas-cache-v3";
 const urlsToCache = [
   "/aplicativo/",
   "/aplicativo/index.html",
   "/aplicativo/manifest.json",
   "/aplicativo/style.css",
   "/aplicativo/icons/icon-192x192.png",
-  "/aplicativo/icons/icon-256x256.png",
-  "/aplicativo/icons/icon-384x384.png",
   "/aplicativo/icons/icon-512x512.png"
 ];
 
-// Instalação do Service Worker
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
-    }).catch((err) => {
+    }).catch(err => {
       console.error("Erro ao adicionar arquivos no cache", err);
     })
   );
   self.skipWaiting();
 });
 
-// Ativação e limpeza de caches antigos
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
@@ -32,7 +28,6 @@ self.addEventListener("activate", (event) => {
   clients.claim();
 });
 
-// Intercepta requisições e responde do cache quando possível
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
