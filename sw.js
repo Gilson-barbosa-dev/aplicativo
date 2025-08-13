27f76e718e7c1eaf22d3bf5492260c6b0e832330
Gilson Barbosa <97996435+Gilson-barbosa-dev@users.noreply.github.com>
2025-08-13 13:34:37 -0300

    create

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener("install", event => {
+  console.log("📥 Service Worker instalado");
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
